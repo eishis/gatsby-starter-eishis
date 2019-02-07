4097b21db16bde5e69b50d1ed003771e82a5c076
UematsuYui <yuikoro12@gmail.com>
2019-02-07 17:09:22 +0900

    add react-fontawesome

---
+++ src/components/IconLibrary.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faTwitter, faGetPocket, faLine } from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faTwitter,
+  faFacebook,
+  faGetPocket,
+  faLine
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import '../components/IconLibrary'
 
 export default class IndexPage extends React.Component {
   render() {
