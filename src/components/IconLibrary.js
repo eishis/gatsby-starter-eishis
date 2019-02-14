5fd23df905596de418319d10d99953aebb1923da
UematsuYui <yuikoro12@gmail.com>
2019-02-14 17:10:23 +0900

    delete IconLibrary.js

--- src/components/IconLibrary.js
+++
@@ -1,9 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFacebook, faTwitter, faGetPocket, faLine } from '@fortawesome/free-brands-svg-icons'
-
-library.add(
-  faTwitter,
-  faFacebook,
-  faGetPocket,
-  faLine
-)
