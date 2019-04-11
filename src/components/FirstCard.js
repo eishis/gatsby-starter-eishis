1ae3d93a2d84d8f04788984d0b3655b1f5e8368a
takashi yamakawa <yamakawa@eishis.com>
2019-04-11 17:24:08 +0900

    SP対応

--- src/components/FirstCard.js
+++ src/components/FirstCard.js
@@ -9,42 +9,57 @@ const styles = {
     composes: 'p-inner',
     display: 'flex',
     justifyContent: 'space-between',
-    '&:hover': {
-      boxShadow: '0 4px 24px rgba(0,0,0,0.12)',
-    },
+    '&:hover': { boxShadow: '0 4px 24px rgba(0,0,0,0.12)' },
+    '@media (max-width: 960px)': { display: 'block' },
   },
   flexInner: {
     width: 'calc( 48% - 24px )',
+    '@media (max-width: 960px)': {
+      width: '100%',
+      marginTop: 16,
+    },
   },
+  
   tagInner: {
     marginTop: 12,
     display: 'flex',
     justifyContent: 'flex-start',
     alignItems: 'center',
+    '@media (max-width: 960px)': { marginTop: 0 }
   },
   inner: {
     display: 'flex',
     justifyContent: 'space-between',
-    alignItems: 'flex-end'
+    alignItems: 'flex-end',
+    '@media (max-width: 960px)': {
+      margin: {
+        top: 16,
+        bottom: 16
+      }
+    }
   },
   title: {
-    fontSize: '1.8em',
+    font: {
+      size: '1.8em',
+      weight: 700
+    },
     overflow: 'hidden',
     whiteSpace: 'nowrap',
     textOverflow: 'ellipsis',
     letterSpacing: 2,
+    '@media (max-width: 960px)': { fontSize: '1.4em' }
   },
   thumbnail: {
-    width: '52%',
+    width: '100%',
+    maxWidth: 480,
     height: 280,
     backgroundColor: '#cccccc',
     borderRadius: 4,
     textAlign: 'center',
     lineHeight: '60px',
+    '@media (max-width: 960px)': { height: 180 }
   },
-  text: {
-    marginTop: 16,
-  }
+  text: { marginTop: 16 }
 }
 
 const Card = props => {
@@ -57,18 +72,18 @@ const Card = props => {
 
   return (
     <Link to={props.content.fields.slug} className={classes.container}>
-      <div style={styles.thumbnail} />
-      <div style={styles.flexInner}>
-        <h3 className="c-ttl" style={styles.title}>
+      <div className={classes.thumbnail} />
+      <div className={classes.flexInner}>
+        <h3 className="c-ttl" className={classes.title}>
           {props.content.frontmatter.title}
         </h3>
-        <div style={styles.inner}>
-          <div style={styles.tagInner}>
+        <div className={classes.inner}>
+          <div className={classes.tagInner}>
             {tags && tags.map(tag => <Tag tag={tag.toUpperCase()} />)}
           </div>
           <Date content={props.content.frontmatter.date} />
         </div>
-        <p style={styles.text}>{props.content.excerpt}</p>
+        <p className={classes.text}>{props.content.excerpt}</p>
       </div>
     </Link>
   )
